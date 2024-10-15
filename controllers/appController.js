4db14fa5acc8afa678771792f3491ac326209466
Mouzone <sunnyliu010@gmail.com>
2024-10-15 18:36:13 -0400

    Write queries function to insert and the logic when sign-up form posts

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,7 +1,16 @@
+const queries = require("../models/queries")
+
 module.exports.signUpGet = (req, res) => {
     res.render("sign-up", {title: "Sign Up"})
 }
 
-module.exports.signUpPost = (req, res) => {
-    
+module.exports.signUpPost = async (req, res) => {
+    await queries.insertUser(
+        req.body.first_name,
+        req.body.last_name,
+        req.body.username,
+        req.body.password
+    )
+
+    res.redirect("/")
 }
--- models/queries.js
+++ models/queries.js
@@ -1,5 +1,9 @@
 const db = require("../config/database")
 
-module.exports.insertUser = async (username, password) => {
-    await db.query("INSERT VALUES (username, password")
+module.exports.insertUser = async (first_name, last_name, username, password) => {
+    await db.query(
+        `INSERT INTO account (first_name, last_name, username, password)
+        VALUES ($1, $2, $3, $4)`,
+        [first_name, last_name, username, password]
+        )
 }
