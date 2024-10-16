80b0daaee28790fbda6fe2f6cc768a2c2dd18fb0
Mouzone <sunnyliu010@gmail.com>
2024-10-16 15:32:00 -0400

    Add passport auhtenticate to login post route

--- routers/appRouter.js
+++ routers/appRouter.js
@@ -1,11 +1,17 @@
 const { Router } = require('express')
 const appRouter = Router()
 const appController = require('../controllers/appController')
+const passport = require('../config/passport')
 
 appRouter.get("/sign-up", appController.signUpGet)
 appRouter.post("/sign-up", appController.signUpPost)
 appRouter.get("/join-club", appController.joinClubGet)
 // appRouter.post("/join-club", appController.joinClubPost)
-appRouter.get("/log-in", appController.logInGet)
-// appRouter.post("/log-in", appController.logInPost)
+appRouter.get("/login", appController.logInGet)
+appRouter.post("/login",
+    passport.authenticate(
+    "local",
+        {successRedirect: "/", failureRedirect: "/"}
+    )
+)
 module.exports = appRouter
