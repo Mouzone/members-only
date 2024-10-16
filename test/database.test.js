d68f87b714fdb3f1bce090f0de8529125bbe12f5
Mouzone <sunnyliu010@gmail.com>
2024-10-15 20:06:48 -0400

    Write test for inserting into account table

--- test/database.test.js
+++ test/database.test.js
@@ -1,12 +1,12 @@
 const { expect } = require('chai');
-const pool = require('../config/database'); 
+const db = require('../config/database');
 
 describe('Database Connection', function () {
     it('should successfully connect to the database', async function () {
         let client;
 
         try {
-            client = await pool.connect();
+            client = await db.connect();
             expect(client).to.not.be.null;
         } catch (error) {
             expect.fail('Failed to connect to the database: ' + error.message);
---
+++ test/queries.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const {insertUser} = require("../models/queries");
+const db = require("../config/database")
+
+describe('Query Account Table', function () {
+    it ('should successfully insert to Account table', async function () {
+        let client;
+
+        try {
+            client = await db.connect()
+            await insertUser("John", "Doe", "johndoe", "securepassword"); // Insert user into the table
+
+            const result = await client.query(`SELECT * FROM account WHERE username = $1`, ["johndoe"])
+            expect(result.rows).to.have.lengthOf(1)
+            expect(result.rows[0]).to.include({ first_name: "John", last_name: "Doe", username: "johndoe" })
+        } catch (error) {
+            expect.fail('Failed to insert into Account table: ' + error.message)
+        } finally {
+            if (client) {
+                await client.query(`DELETE FROM account WHERE username = $1`, ["johndoe"])
+                client.release();
+            }
+        }
+    })
+})
