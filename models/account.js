f7b75a14f61cf99cd2177ff5008c3a5d6740082e
Mouzone <sunnyliu010@gmail.com>
2024-10-17 12:09:39 -0400

    Refactor queries to only return columns of importance

--- models/account.js
+++ models/account.js
@@ -5,24 +5,24 @@ exports.insertUser = async (first_name, last_name, username, password, membershi
         `INSERT INTO account (first_name, last_name, username, password, membership_id)
         VALUES ($1, $2, $3, $4, $5)`,
         [first_name, last_name, username, password, membership_id]
-        )
+    )
 }
 
 exports.findByUsername = async (username) => {
     const { rows } = await db.query(
-        `SELECT * FROM account
-        WHERE username = $1`,
-        [username]
-    )
+                        `SELECT 1 FROM account
+                        WHERE username = $1`,
+                        [username]
+                    )
     return rows
 }
 
 exports.findById = async (account_id) => {
     const { rows } = await db.query(
-        `SELECT * FROM account
-        WHERE account_id = $1`,
-        [account_id]
-    )
+                        `SELECT * FROM account
+                        WHERE account_id = $1`,
+                        [account_id]
+                    )
 
     return rows
 }
@@ -38,10 +38,10 @@ exports.updateToMember = async (account_id) => {
 
 exports.getIdFromUsername = async (username) => {
     const { rows } = await db.query(
-        `SELECT account_id FROM account
-        WHERE username = $1`,
-        [username]
-    )
+                        `SELECT account_id FROM account
+                        WHERE username = $1`,
+                        [username]
+                    )
 
     return rows
 }
